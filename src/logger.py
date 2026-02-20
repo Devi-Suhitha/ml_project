7bacc636dbc13512bad6e303d52be9a3e3b36481
Devi-Suhitha <suhithareddykesari@gmail.com>
2026-02-20 12:36:50 +0530

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -13,5 +13,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-if __name__ == "__main__":
-    logging.info("Logging has started")
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
